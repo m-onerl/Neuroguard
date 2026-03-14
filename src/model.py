f81647d5c6f2554897784778cc82525f094293fd
m-onerl <monerl2525@gmail.com>
2026-03-14 21:15:12 +0100

    chore: refactor of project

--- src/model.py
+++ src/model.py
@@ -4,8 +4,7 @@ import torch.optim as optim
 import numpy as np
 import logging
 
-from sklearn.preprocessing import StandardScaler
-from torch.utils.data import DataLoader, TensorDataset
+from train import train
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -26,40 +25,7 @@ class NeuroGuard(nn.Module):
         x = torch.relu(self.layer1(x))
         x = torch.relu(self.layer2(x))
         return self.sigmoid(self.out(x))
-    
-def train():
-    
-    x_raw = np.load('./data/processed/X_train.npy')
-    y_raw = np.load('./data/processed/y_train.npy')
-
-    scaler = StandardScaler()
-    x_normalized = scaler.fit_transform(x_raw)
-
-    X_tensor = torch.from_numpy(x_normalized).float()
-    y_tensor = torch.from_numpy(y_raw).float().reshape(-1, 1)
-    
-    dataset = TensorDataset(X_tensor, y_tensor)
-    train_loader = DataLoader(dataset, batch_size = 64, shuffle = True)
-    
-    model = NeuroGuard(input_dim = X_tensor.shape[1])
-    criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr = 0.001)
-    
-    epochs = 10
-    
-    
-    for epoch in range(epochs):
-        running_loss = 0.0
-        
-        for batch_X, batch_y in train_loader:
-            optimizer.zero_grad()
-            predictions = model(batch_X)
-            loss = criterion(predictions, batch_y)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            
-        logger.info(f'Epoch {epoch+1}/{epochs} Average of loss {running_loss/len(train_loader)}')
+  
             
 if __name__ == "__main__":
     train()
