8b8fd0f28c348fea8d98cdfde9567d6c1911f3e7
m-onerl <monerl2525@gmail.com>
2026-03-30 22:53:37 +0200

    chore: added button for preproces data trying to repair path to directory

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,15 +1,22 @@
 import tkinter as tk
-
+from preprocess import DataPreprocesor
 
 class GUINeuroguard(tk.Tk):
     def __init__(self):
         super().__init__()
         
         self.title("NeuroGuard")
-        self.geometry("600x400")
-        
+        self.geometry("600x400")     
         self.label = tk.Label(self, text = 'NEUROGUARD')
-
+        self.label.pack(pady=20)
+        
+        self.button = tk.Button(
+            self, 
+            text = 'Preproces Data', 
+            width = 25, 
+            command = lambda: DataPreprocesor.preprocesor('KDDTrain+.txt', 'processed')
+        )
+        self.button.pack(pady=10)
 if __name__ == "__main__":
     
     app = GUINeuroguard()
