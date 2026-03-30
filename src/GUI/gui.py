cf2d2522d1ec50f09f3a32ac8ac02229e7c83fed
m-onerl <monerl2525@gmail.com>
2026-03-30 23:10:08 +0200

    chore: write button function for gui

--- src/GUI/gui.py
+++ src/GUI/gui.py
@@ -1,5 +1,13 @@
 import tkinter as tk
 from ..preprocess import DataPreprocesor
+from src.train import Training
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s %(name)s = %(levelname)s - %(message)s'
+)
 
 class GUINeuroguard(tk.Tk):
     def __init__(self):
@@ -14,9 +22,33 @@ class GUINeuroguard(tk.Tk):
             self, 
             text = 'Preproces Data', 
             width = 25, 
-            command = lambda: DataPreprocesor.preprocesor('KDDTrain+.txt', 'processed')
+            command = self.preprocess_data()
         )
         self.button.pack(pady=10)
+        
+        self.button = tk.Button(
+            self, 
+            text = 'Train model', 
+            width = 25, 
+            command = self.train_model()
+        )
+        self.button.pack(pady=10)
+        
+    def preprocess_data(self):
+        try:
+            DataPreprocesor.preprocesor('KDDTrain+.txt', 'processed')
+        except Exception as e:
+            logging.error(f'Error: {e}')
+        
+    def train_model(self):
+        try:
+            trainer = Training()
+            trainer.train()
+        except Exception as e:
+            logging.error(f'Error{e}')
+            
+            
+            
 if __name__ == "__main__":
     
     app = GUINeuroguard()
