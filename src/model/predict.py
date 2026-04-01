1c191ff0b38c018b47495f402655bf5450d92f5c
m-onerl <monerl2525@gmail.com>
2026-04-01 21:14:02 +0200

    chore: added device to predict model and scaler load them assign model from neuroguard class make mean and scale from data scaler

--- src/model/predict.py
+++ src/model/predict.py
@@ -1,9 +1,10 @@
 import numpy as np
-
-from .preprocess import DataPreprocesor
-
-
+import torch
 import logging
+
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from .init_model import NeuroGuard
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -11,11 +12,49 @@ logging.basicConfig(
     format='%(asctime)s %(name)s = %(levelname)s - %(message)s'
 )
 
+CLASS_NAMES = {
+    0: "Normal",
+    1: "DoS",
+    2: "Probe",
+    3: "R2L",
+    4: "U2R",
+}
+
+
 class Predictor:
-    def __init__(self, model_path: str = "./artifacts/neuroguard_model.pt", scaler_path: str = "./artifacts/scaler.npz"):
-        self.model_path = model_path
-        self.scaler_path = scaler_path
-        
-        
-    def load_model(self):
-        logger.info("Loading model and scaler ...")
+    def __init__(
+        self,
+        model_path: str = "./artifacts/neuroguard_model.pt",
+        scaler_path: str = "./artifacts/scaler.npz"
+    ):
+        self.model_path = Path(model_path)
+        self.scaler_path = Path(scaler_path)
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = None
+        self.scaler = None
+        self._load_artifacts()
+
+    def _load_artifacts(self):
+        logger.info("Loading model and scaler...")
+
+        if not self.model_path.exists() or not self.scaler_path.exists():
+            raise FileNotFoundError(
+                f"Artifacts not found. You have to train the model first.\n"
+            )
+
+        state_dict = torch.load(self.model_path, map_location=self.device)
+        input_dim = state_dict['network.0.weight'].shape[1]
+        num_classes = state_dict['network.11.weight'].shape[0]
+
+        self.model = NeuroGuard(input_dim=input_dim, num_classes=num_classes)
+        self.model.load_state_dict(state_dict)
+        self.model.to(self.device)
+        self.model.eval() 
+        scaler_data = np.load(self.scaler_path)
+        self.scaler = StandardScaler()
+        self.scaler.mean = scaler_data['mean']
+        self.scaler.scale = scaler_data['scale']
+
+        logger.info("Model and scaler loaded successfully. input_dim=%d, num_classes=%d",
+                    input_dim, num_classes)
+
